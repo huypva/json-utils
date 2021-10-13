551d41a36d1d79da9a7367b86815b387183b7c05
huypva <huypva@vng.com.vn>
2021-10-13 09:43:11 +0700

    Add images

--- json-utils/src/main/java/io/codebyexample/jsonutils/utils/GsonUtils.java
+++ json-utils/src/main/java/io/codebyexample/jsonutils/utils/GsonUtils.java
@@ -2,12 +2,7 @@ package io.codebyexample.jsonutils.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-import java.io.IOException;
-import java.util.Base64;
 
 /**
  * @author huypva
--- json-utils/src/main/java/io/codebyexample/jsonutils/utils/JacksonUtils.java
+++ json-utils/src/main/java/io/codebyexample/jsonutils/utils/JacksonUtils.java
@@ -3,7 +3,6 @@ package io.codebyexample.jsonutils.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonParseException;
 
 /**
  * @author huypva
